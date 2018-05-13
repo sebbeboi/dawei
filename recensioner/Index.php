a628a5f1161a10724a65d1350f3249c7450696da
JaoK <joakim.ohl@gmail.com>
2018-05-13 02:19:56 +0200

    början till sidan recensioner

--- recensioner/Index.php
+++ recensioner/Index.php
@@ -12,13 +12,44 @@
 
 
 <div id="review_form_container">
+<?php
 
-<form id="review_form">
+    $dbc = mysqli_connect();
+    
+    if(isset($_POST['author']) && isset($_POST['text']) && isset($_POST['stars'])){
+       
+        
+        $author = $_POST['author'];
+        $text = $_POST['text'];
+        $stars = $_POST['stars'];
+        
+        $query = "INSERT INTO reviews (author,text,stars) VALUES ('$author','$text','$stars');";
+
+        if(mysqli_query($dbc,$query)){
+            
+            ?>
+            <p id="thanks">Tack för din recension!</p>
+            <?php
+            
+        }
+        else{
+            ?>
+            <p id="thanks">Ett fel inträffade med din recension!</p>
+            <?php
+        }
+    
+    }else{
+    
+    
+?>
+
+
+<form id="review_form" method="POST">
 
 Ditt namn: <input type="text" name="author"/><br>
 Recension: <input type="textfield" name="text"/><br>
 Betyg: <input id="slider" type="range" min="1" max="5" step="1" name="stars"/><span id="value">1</span><br>
-<input type="submit" />
+<input type="submit" value="Skicka"/>
 </div>
 
 <script>
@@ -33,11 +64,29 @@ slider.oninput = function (){
 
 
 </script>
-
+<?php
+}
+?>
 
 
 <div id="reviews_container">
+<p id="reviews_headline"> Våra 10 senaste recensioner:</p>
+<?php
 
+    $query = "SELECT * FROM reviews ORDER BY reviews_id DESC LIMIT 10;";
+    
+    $result = mysqli_query($dbc,$query);
+    
+    while($row = mysqli_fetch_array($result)){
+        
+        echo $row['reviews_text'] . "<br>";
+        
+    }
+    
+    
+    
+    
+?>
 
 </div>
     
