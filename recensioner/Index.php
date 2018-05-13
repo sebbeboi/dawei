7649adec25a6656ed2a0784ebc3ab021fd66cd15
JaoK <joakim.ohl@gmail.com>
2018-05-13 02:03:21 +0200

    formulär för recension

--- recensioner/Index.php
+++ recensioner/Index.php
@@ -9,6 +9,24 @@
 <?php
     include("../templates/navigation.php");
 ?>
+
+
+<div id="review_form_container">
+
+<form id="review_form">
+
+Ditt namn: <input type="text" name="author"></input><br>
+Recension: <input type="textfield" name="text"></input><br>
+Betyg: <input type="range" min="1" max="5" step="1" name="stars"></input><br>
+
+</div>
+
+
+
+<div id="reviews_container">
+
+
+</div>
     
     
     
