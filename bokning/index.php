30ae6aa18542e1cd4159757b52ab80d11ea2ed1c
sebbeboi <sebastian.arvidsson.it16@gmail.com>
2018-05-17 10:21:51 +0200

    La till text i koden

--- bokning/index.php
+++ bokning/index.php
@@ -1,10 +1,39 @@
+
+<style>
+input[type=text], select {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+}
+
+input[type=submit] {
+    width: 100%;
+    background-color: #4CAF50;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+}
+
+input[type=submit]:hover {
+    background-color: #45a049;
+}
+</style> 
+
+
 <?php
 
-include("../templates/navigation.php");
+include("../templates/navigation.php"); // hämtar NAVen från sökvägen dawei
 
 $dbc = mysqli_connect("localhost","root","","dawei");
 
-if (isset($_POST['name']) && (isset ($_POST['date'])) && (isset($_POST['time'])) && isset($_POST['persons'])){
+if (isset($_POST['name']) && (isset ($_POST['date'])) && (isset($_POST['time'])) && isset($_POST['persons'])){ // om något i form så kommer den skicka vidare det till databasen 
     
     $antal = $_POST['persons'];
     
@@ -12,7 +41,7 @@ if (isset($_POST['name']) && (isset ($_POST['date'])) && (isset($_POST['time']))
 
     $result = mysqli_query($dbc,$query);
     
-    if ($table = mysqli_fetch_array($result)){
+    if ($table = mysqli_fetch_array($result)){ // detta ser till att allt ska vara det det ska vara smil eskde 
         $namn = $_POST['name'];
         $tid = $_POST['time'];
         $datum = $_POST['date'];
@@ -27,7 +56,7 @@ if (isset($_POST['name']) && (isset ($_POST['date'])) && (isset($_POST['time']))
        
     }
         else{
-            echo "inga bord lediga";
+            echo "inga bord lediga"; // I fall inga bord finns lediga i batabasen
         }
         
        
